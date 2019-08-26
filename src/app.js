84b775dafd910a6cb9e78e10c39e80da8cd5d916
Kyle <kirvine87@gmail.com>
2019-08-26 16:57:47 +0100

    final commit

--- src/app.js
+++ src/app.js
@@ -4,19 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      items: [{name: "Buy Shopping", priority: false},
-      {name: "Clean Bathroom", priority: true},
-      {name: "Car\'s MOT", priority: false}],
-      newToDo: ""
+      items: [{name: "Buy Shopping", priority: "low", status: false},
+      {name: "Clean Bathroom", priority: "high", status: false},
+      {name: "Car\'s MOT", priority: "high", status: false}],
+      newToDo: "",
+      newPriority: ""
     },
     methods: {
       saveNewItem: function() {
         if (this.newToDo !== "") {
-          const itemToAdd = {name: this.newToDo, priority: false};
+          const itemToAdd = {name: this.newToDo, priority: this.newPriority, status: false};
           this.items.push(itemToAdd);
           this.newToDo = "";
+          this.newPriority = "";
         };
-      }
+      },
+
+      taskCompleted: function(index) {
+        this.items[index].status = true;
+        this.items[index].name = `${this.items[index].name}:Done`
+      },
+
     }
 
   });
